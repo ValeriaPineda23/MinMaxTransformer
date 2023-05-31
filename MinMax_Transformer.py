f385b13f54b1e58a907123050df44fa87d0dd6eb
Valeria Pineda <Valeria.Pineda@Resideo.com>
2023-05-31 15:14:25 -0600

    Add files via upload

---
+++ MinMax_Transformer.py
@@ -0,0 +1,36 @@
+from h2oaicore.systemutils import segfault, loggerinfo, main_logger
+from h2oaicore.transformer_utils import CustomTransformer
+import datatable as dt
+import numpy as np
+import pandas as pd
+import logging
+
+class MinMaxTransformer(CustomTransformer):
+	_regression = True
+	_binary = True
+	_multiclass = True
+	_numeric_output = True
+	_is_reproducible = True
+	_modules_needed_by_name = ["custom_package==1.0.0"]
+
+	@staticmethod
+	def do_acceptance_test():
+		return True
+
+	@staticmethod
+	def get_default_properties():
+		return dict(col_type = "numeric", min_cols = 1, max_cols = 1, relative_importance = 1)
+
+	def fit_transform(self, X: dt.Frame, y: np.array = None):
+		X_pandas = X.to_pandas()
+		Xmin = min(X_pandas)
+		Xmax = max(X_pandas)
+		X_minmax = (X_pandas-Xmin)/(Xmax-Xmin)
+		return X_minmax
+
+	def transform(self, X: dt.Frame):
+		X_pandas = X.to_pandas()
+		Xmin = min(X_pandas)
+		Xmax = max(X_pandas)
+		X_minmax = (X_pandas-Xmin)/(Xmax-Xmin)
+		return X_minmax
